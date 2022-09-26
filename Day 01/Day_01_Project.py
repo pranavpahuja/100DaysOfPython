bc4670961548ca3fd1b47effdded274bfe777dbb
Pranav Pahuja <prnvp@protonmail.ch>
2022-09-26 12:35:24 +0530

    Basics
    Basics

---
+++ Day 01/Day_01_Project.py
@@ -0,0 +1,11 @@
+print("Hello, welcome to the brand name generator!")
+
+city = input("Which city did you grow up in?")
+
+print("Ah, {}, nice!".format(city))
+
+pet = input("What's the name of your pet?")
+
+print("Ah, hello there, {}!".format(pet))
+
+print("Well, I think a good name for your brand can be:\n" + city + " " + pet + "\nLiked it?")
