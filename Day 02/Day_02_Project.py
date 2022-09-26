a2a0bd349222a9f72ce6b661ed9583b9600928d7
Pranav Pahuja <20995301+pranavpahuja@users.noreply.github.com>
2022-09-26 12:47:30 +0530

    New files

--- Day 02/Day_02_Project.py
+++ Day 02/Day_02_Project.py
@@ -1,5 +1,13 @@
 print("Welcome to the Tip Calculator!")
 
-total_amount = input("What was your total bill amount?")
+total_amount = float(input("What was your total bill amount?"))
 
-no_of_people = input("How many people to split the bill amongst?")
+no_of_people = float(input("How many people to split the bill amongst?"))
+
+tip_perc = float(input("How much % tip do you want to give?"))
+
+total_amt_inc_tip = total_amount + (total_amount*(tip_perc/100))
+
+each_amount = total_amt_inc_tip/no_of_people
+
+print("Each share comes to about: ${} - thanks for using the service!".format(each_amount))
