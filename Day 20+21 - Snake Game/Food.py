f81bda1cc20cee4de1ccae4c9fbdb4ca64d8f03d
Pranav <pranavpahuja@Pranavs-MacBook-Pro.local>
2024-12-20 20:13:54 +0530

    updates

--- Day 20+21 - Snake Game/Food.py
+++ Day 20+21 - Snake Game/Food.py
@@ -1,4 +1,5 @@
 import random
+
 from turtle import Turtle
 
 class Food(Turtle):
@@ -15,6 +16,7 @@ class Food(Turtle):
         self.goto(random_x,random_y)
         #self.food_eaten()
 
+#Refresh
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
